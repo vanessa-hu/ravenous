bbf696a2ff334733bd456440ad87b1c41dd6d2d1
Vanessa Hu <vanessahu248@gmail.com>
2020-08-31 17:13:28 -0400

    finished part 3

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import './Business.css';
-let business = {
-    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-};
+
 
 class Business extends React.Component {
     render() {
@@ -22,14 +12,14 @@ class Business extends React.Component {
         <h2>MarginOtto Pizzeria</h2>
         <div className="Business-information">
             <div className="Business-address">
-            <p>{business.address}</p>
-            <p>{business.city}</p>
-            <p>{business.state + " " + business.zipCode}</p>
+            <p>{this.props.business.address}</p>
+            <p>{this.props.business.city}</p>
+            <p>{this.props.business.state + " " + this.props.business.zipCode}</p>
             </div>
             <div className="Business-reviews">
             <h3>ITALIAN</h3>
-        <h3 className="rating">{business.rating + " stars"}</h3>
-            <p>{business.reviewCount + "reviews"}</p>
+        <h3 className="rating">{this.props.business.rating + " stars"}</h3>
+            <p>{this.props.business.reviewCount + "reviews"}</p>
             </div>
         </div>
         </div>
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,18 +3,66 @@ import './SearchBar.css';
 
 // https://www.yelp.com/developers/documentation/v3/business_search
 
-const sortByOptions = {
-    'Best Match': 'best_match',
-    'Highest Rated': 'rating',
-    'Most Reviewed': 'review_count'
-}
+
 
 class SearchBar extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            'term': '',
+            'location': '',
+            'sortBy': 'best_match'
+        };
+        // member variable
+        this.sortByOptions = {
+            'Best Match': 'best_match',
+            'Highest Rated': 'rating',
+            'Most Reviewed': 'review_count'
+        };
+
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+
+    }
+    //checks whether a sort by option is the chosen one by the user (or default)
+    getSortByClass(sortByOption) {
+        if (this.state.sortBy === sortByOption) {
+            return 'active';
+        }
+        else {
+            return '';
+        }
+    }
+    handleSortByChange(sortByOption) {
+        this.setState({sortBy:sortByOption});
+        //handles binding in renderSortByOptions?
+    }
+    handleTermChange(e) {
+        this.setState({
+            term: e.target.value
+        });
+    }
+    handleLocationChange(e) {
+        this.setState({
+            location: e.target.value
+        });
+    }
+
+    handleSearch(e) {
+        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+        e.preventDefault();
+    }
+
     renderSortByOptions() {
-        return Object.keys(sortByOptions).map(sortByOption => {
-            const sortByOptionValue = sortByOptions[sortByOption];
+        return Object.keys(this.sortByOptions).map(sortByOption => {
+            let sortByOptionValue = this.sortByOptions[sortByOption];
             // key: https://stackoverflow.com/questions/40668654/in-react-what-is-the-function-of-the-key-attribute-in-html-tag
-        return <li key={sortByOptionValue}>{sortByOption}</li>
+        return <li 
+                    onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
+                    key={sortByOptionValue}
+                    className={this.getSortByClass(sortByOptionValue)}>{sortByOption}</li>
         } );
     }
 
@@ -23,14 +71,14 @@ class SearchBar extends React.Component {
             <div className="SearchBar">
                 <div className="SearchBar-sort-options">
                     <ul>
-                        {this.renderSortByOptions}
+                        {this.renderSortByOptions()}
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
-                    <input placeholder="Search Businesses" />
-                    <input placeholder="Where?" />
+                    <input onChange={this.handleTermChange} placeholder="Search Businesses" />
+                    <input onChange={this.handleLocationChange} placeholder="Where?" />
                 </div>
-                <div className="SearchBar-submit">
+                <div className="SearchBar-submit" onClick={this.handleSearch}>
                     <a>Let's Go</a>
                 </div>
             </div>
